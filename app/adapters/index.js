9182b5ef7e0ef7581af6a075e8aadd9eaa934f73
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-02-02 13:06:21 -0700

    Revert "Setup Ember Data"
    This reverts commit 6210232a19b7f6fb9f1c18f074bafc7616d86125.

--- app/adapters/index.js
+++
@@ -1,6 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.JSONAPIAdapter.extend({
-  // host: 'https://immense-ridge-96107.herokuapp.com',
-  host: 'http://localhost:3000/quotes'
-});
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    // return $.getJSON('quotes.json');
-    return this.get('store').findAll('quote');
+    return $.getJSON('quotes.json');
   }
 });
