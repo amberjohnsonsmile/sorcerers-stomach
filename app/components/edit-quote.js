e276055328bb78a2a051afc0a7801daeec1deed6
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-02-19 19:37:51 -0700

    Finish post functionality

--- app/components/edit-quote.js
+++ app/components/edit-quote.js
@@ -1,20 +1,22 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  // submit(event) {
-  //   event.preventDefault();
-  //   console.log(event.target.quote.value)
-  //   fetch('https://immense-ridge-96107.herokuapp.com/quotes', {
-  //     method: 'PUT',
-  //     body: JSON.stringify({
-  //       id: params.id,
-  //       quote: event.target.quote.value
-  //     }),
-  //     headers: new Headers({
-  //       'Content-Type': 'application/json'
-  //     })
-  //   })
-  //     .then(response => response.json())
-  //     .catch(console.error);
-  // }
+  submit(event) {
+    event.preventDefault();
+    let quoteData = this.$('textarea')[0];
+    fetch('https://immense-ridge-96107.herokuapp.com/quotes/' + this.$('textarea')[0].id, {
+      method: 'PUT',
+      body: JSON.stringify({
+        id: quoteData.id,
+        quote: quoteData.value
+      }),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    })
+      .then(response => response.json())
+      .catch(console.error);
+    quoteData.value = '';
+    this.$('.edited').removeClass('hidden');
+  }
 });
