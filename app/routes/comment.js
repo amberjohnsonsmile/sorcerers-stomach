d9cd51a011a1ff97bfa7522012d3927b38452f64
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-02-21 14:44:57 -0700

    Display comments with a quote

--- app/routes/comment.js
+++ app/routes/comment.js
@@ -2,8 +2,13 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    return fetch('https://immense-ridge-96107.herokuapp.com/quotes/' + params.id)
-    .then(response => response.json())
-    .catch(console.error);
+    return Ember.RSVP.hash({
+      quotes: fetch('https://immense-ridge-96107.herokuapp.com/quotes/' + params.id)
+        .then(response => response.json())
+        .catch(console.error),
+      comments: fetch('https://immense-ridge-96107.herokuapp.com/quote-comments/' + params.id)
+        .then(response => response.json())
+        .catch(console.error)
+    });
   }
 });
