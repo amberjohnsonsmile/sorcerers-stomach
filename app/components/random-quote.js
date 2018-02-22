54f9bf1a253fc8a29c48f74a016a205602a2e376
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-02-22 09:44:21 -0700

    Remove RANDOM link

--- app/components/random-quote.js
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component';
-
-export default Component.extend({
-  submit(event) {
-    event.preventDefault();
-    $('.random').removeClass('hidden');
-    $('html').css('backgroundImage', 'url("https://images.unsplash.com/photo-1516703914899-e8303d01329a")')
-  }
-});
