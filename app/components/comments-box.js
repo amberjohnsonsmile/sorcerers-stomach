98847fe886109bee37429958c64014c0a2bed803
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-02-22 11:54:35 -0700

    Add delete button and server functionality

--- app/components/comments-box.js
+++ app/components/comments-box.js
@@ -1,4 +1,24 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  click(event) {
+    event.preventDefault();
+    // if (event.target.textContent == 'DELETE') {
+    //   let commentId = this.$(event.target).attr('class');
+    //   fetch('https://immense-ridge-96107.herokuapp.com/comments/' + commentId, {
+    //     method: 'DELETE',
+    //     headers: new Headers({
+    //       'Content-Type': 'application/json'
+    //     })
+    //   })
+    //     .catch(console.error);
+    //   // if there is only one <li>, delete the whole <ul>
+    //   this.$(event.target).closest('li').addClass('hidden');
+    //   // else delete only the <li>
+    // }
+    console.log($('li').length);
+    if ($('li').length == 1 || $('li').length == 2) {
+    
+    }
+  }
 });
